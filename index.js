5088c5d820c38c5bbd53838d6827a44a02df0681
Dmitrii Sandakov <marqpeo@gmail.com>
2023-05-28 14:04:45 +0200

    28.05.23 - intermediate commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import Fastify from "fastify";
+import routes from "./lib/routes/index.js";
+
+const port = process.env.DEV_MODE ? 4000 : 3000;
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.addHook('preHandler', (req,reply, done) => {
+  reply.header('Access-Control-Allow-Origin', '*');
+  reply.header('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  reply.header('Access-Control-Max-Age', 2592000);
+  done();
+})
+
+fastify.register(routes);
+
+
+fastify.listen({ port: 4000 }, function(err, address){
+  // console.log(process.env.DEV_MODE);
+  if(err){
+    fastify.log.error(err);
+    process.exit(1);
+  }
+})
